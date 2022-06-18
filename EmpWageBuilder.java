c4858106313142b92df3d0ab28769bf2026a512c
Shubom123 <shubomnath365@gmail.com>
2022-06-19 02:30:11 +0530

    Added: Calculating total employee wages added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,10 +2,13 @@ public class EmpWageBuilder{
 public static final int IS_FULL_TIME=1;
 public static final int IS_PART_TIME=2;
 public static final int empWagePerHour=20;
+public static final int NUM_OF_WORKING_DAYS=20;
 public static void main(String args[]){
 System.out.println("Welcome to Employee Wage Builder");
 int empWage=0;
 int empHours=0;
+int totalEmpWage=0;
+for(int days=0; days<=NUM_OF_WORKING_DAYS; days++){
 int empCheck = (int) Math.floor(Math.random()*10)%3;
 switch (empCheck){
 case IS_FULL_TIME:
@@ -21,6 +24,9 @@ System.out.println("Employee is absent");
 empHours=0;
 }
 empWage=empWagePerHour*empHours;
+totalEmpWage=totalEmpWage+empWage;
 System.out.println("Wage of a day is: "+ empWage);
+}
+System.out.println("Wage of whole month is: "+ totalEmpWage);
 }
 }
