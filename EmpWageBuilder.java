4909bc3e300a1cbd9ec349f4bd528429bcf9249c
Shubom123 <shubomnath365@gmail.com>
2022-06-19 00:01:08 +0530

    Added: Daily Wage functionality added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,12 +2,19 @@ public class EmpWageBuilder{
 public static void main(String args[]){
 System.out.println("Welcome to Employee Wage Builder");
 int IS_FULL_TIME=1;
+int empWage=0;
+int empWagePerHour=20;
+int empHours=0;
 double empCheck = Math.floor(Math.random()*10)%2;
 if (empCheck == IS_FULL_TIME){
 System.out.println("Employee is present");
+empHours=8;
 }
 else{
 System.out.println("Employee is absent");
+empHours=0;
 }
+empWage=empWagePerHour*empHours;
+System.out.println("Wage of a day is: "+ empWage);
 }
 }
