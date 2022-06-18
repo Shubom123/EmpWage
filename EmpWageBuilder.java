a6800cd8d8f2a1a42035641c8e5cb3b22d7f226c
Shubom123 <shubomnath365@gmail.com>
2022-06-19 03:07:47 +0530

    Employee Functionality added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,12 +3,16 @@ public static final int IS_FULL_TIME=1;
 public static final int IS_PART_TIME=2;
 public static final int empWagePerHour=20;
 public static final int NUM_OF_WORKING_DAYS=20;
+public static final int MAX_HOURS_IN_MONTH=100;
 public static void main(String args[]){
 System.out.println("Welcome to Employee Wage Builder");
 int empWage=0;
 int empHours=0;
+int totalEmpHours=0;
+int totalWorkDays=0;
 int totalEmpWage=0;
-for(int days=0; days<=NUM_OF_WORKING_DAYS; days++){
+while (totalEmpHours<=MAX_HOURS_IN_MONTH && 
+totalWorkDays<=NUM_OF_WORKING_DAYS){
 int empCheck = (int) Math.floor(Math.random()*10)%3;
 switch (empCheck){
 case IS_FULL_TIME:
@@ -23,10 +27,10 @@ default:
 System.out.println("Employee is absent");
 empHours=0;
 }
-empWage=empWagePerHour*empHours;
-totalEmpWage=totalEmpWage+empWage;
-System.out.println("Wage of a day is: "+ empWage);
+totalEmpHours = totalEmpHours + empHours;
+System.out.println(" Emp Hrs: " + empHours);
 }
+totalEmpWage = totalEmpHours * empWagePerHour;
 System.out.println("Wage of whole month is: "+ totalEmpWage);
 }
 }
