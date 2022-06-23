1d0852cec023b82dc3d1ba8a7380460c961f4655
Shubom123 <shubomnath365@gmail.com>
2022-06-23 17:20:51 +0530

    Added: Classes and method variables added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,17 +4,22 @@ public static final int IS_PART_TIME=2;
 public static final int empWagePerHour=20;
 public static final int NUM_OF_WORKING_DAYS=20;
 public static final int MAX_HOURS_IN_MONTH=100;
-public static void main(String args[]){
-System.out.println("Welcome to Employee Wage Builder");
+
+public static void EmployeeWage()
+{
 int empWage=0;
 int empHours=0;
 int totalEmpHours=0;
 int totalWorkDays=0;
 int totalEmpWage=0;
-while (totalEmpHours<=MAX_HOURS_IN_MONTH && 
-totalWorkDays<=NUM_OF_WORKING_DAYS){
+System.out.println("Welcome to Employee Wage Builder");
+
+while (totalEmpHours<=MAX_HOURS_IN_MONTH && totalWorkDays<NUM_OF_WORKING_DAYS)
+{
+totalWorkDays++;
 int empCheck = (int) Math.floor(Math.random()*10)%3;
-switch (empCheck){
+switch (empCheck)
+{
 case IS_FULL_TIME:
 System.out.println("Employee is present");
 empHours=8;
@@ -29,8 +34,18 @@ empHours=0;
 }
 totalEmpHours = totalEmpHours + empHours;
 System.out.println(" Emp Hrs: " + empHours);
+empWage = empWagePerHour * empHours;
+System.out.println("Wage of a day is: "+ empWage);
 }
 totalEmpWage = totalEmpHours * empWagePerHour;
 System.out.println("Wage of whole month is: "+ totalEmpWage);
+System.out.println("Number of hour worker in whole month is: "+ totalEmpHours);
+System.out.println("Number of days worker in whole month is: "+ totalWorkDays);
+
+}
+
+public static void main(String args[])
+{
+ EmployeeWage();
 }
 }
